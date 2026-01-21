f4a0e2f35021bb5790a38a5d88f1e98b38b1e166
konoan30 <ska866234@gmail.com>
2026-01-21 11:52:48 +0200

    Add project files.

---
+++ Project6/FileName.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <windows.h>
+#include <fstream>
+using namespace std;
+
+
+mutex m;
+
+void saveFile()
+{
+	m.lock();
+	{
+		ofstream out("file.txt", ios::trunc);
+		if (out.is_open())
+		{
+			for (size_t i = 1; i < 100; i++)
+			{
+				i = rand() % 100;
+				out << i << endl;
+			}
+			out.close();
+		}
+		m.unlock();
+	}
+}
+void readFile()
+{
+
+	m.lock();
+	ifstream in("file.txt");
+	int num;
+	if (in. is_open())
+	{
+		while (in >> num)
+		{
+			cout << num << endl;
+			Sleep(200);
+		}
+	}
+	m.unlock();
+}
+int main(   )
+{
+   
+
+
+
+
+}
